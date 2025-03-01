0d06417503202679594fd317e3298d73030b3e70
Skycoder42 <Skycoder42@users.noreply.github.com>
2025-03-01 08:30:07 +0100

    format

--- web/main.dart
+++ web/main.dart
@@ -6,9 +6,9 @@ import 'package:lzstring/lzstring.dart';
 import 'package:web/web.dart';
 
 Future<void> main() async {
-  final textAreaElement = (document.querySelector('#output')!
-      as HTMLTextAreaElement)
-    ..value = 'Loading, please wait...';
+  final textAreaElement =
+      (document.querySelector('#output')! as HTMLTextAreaElement)
+        ..value = 'Loading, please wait...';
 
   final query = URLSearchParams(window.location.search.toJS);
   final source = query.get('i');
@@ -18,15 +18,17 @@ Future<void> main() async {
   }
 
   try {
-    final decodedStats =
-        await LZString.decompressFromEncodedURIComponent(source);
+    final decodedStats = await LZString.decompressFromEncodedURIComponent(
+      source,
+    );
     if (decodedStats == null) {
       textAreaElement.value = 'Failed to decode stats';
       return;
     }
 
-    final prettyStats =
-        const JsonEncoder.withIndent('  ').convert(json.decode(decodedStats));
+    final prettyStats = const JsonEncoder.withIndent(
+      '  ',
+    ).convert(json.decode(decodedStats));
 
     textAreaElement
       ..value = prettyStats
